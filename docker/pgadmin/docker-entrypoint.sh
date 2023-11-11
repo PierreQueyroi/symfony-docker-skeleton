89ec1d70d7e9aed36ddba61f652e0e419a973c8b
Pierre QUEYROI <pierre@ekwi.tech>
2023-11-11 18:25:31 +0100

    Initial commit

--- docker/pgadmin/docker-entrypoint.sh
+++ docker/pgadmin/docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "$DATABASE_HOST:$DATABASE_PORT:$DATABASE_NAME:$DATABASE_USR:$DATABASE_PWD" 
 echo "$DATABASE_HOST:$DATABASE_PORT:postgres:$DATABASE_USR:$DATABASE_PWD" >> /pgadmin4/pgpass
 echo "{'Servers':{'1':{'Name':'Postgres DB','Port':$DATABASE_PORT,'Username': '$DATABASE_USR','Group':'Servers','Host':'db','SSLMode':'prefer','PassFile':'/pgpass','MaintenanceDB':'$DATABASE_NAME'}}}" > /pgadmin4/servers.json
 
-PGADMIN_USER=${$PGADMIN_USER | sed 's/@/_/g'}
+PGADMIN_USER=${PGADMIN_USER | sed 's/@/_/g'}
 
 mkdir -m 700 /var/lib/pgadmin/storage/${PGADMIN_USER}
 chown -R pgadmin:root /var/lib/pgadmin/storage/${PGADMIN_USER}
